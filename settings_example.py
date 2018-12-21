e5948ba6c4259a21b5e1740d4221986e0fce083c
yangmv <yangmingwei@shinezone.com>
2018-12-21 13:31:10 +0800

    set settings

---
+++ settings_example.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#encoding:utf-8
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+app_settings = {
+    "template_path": ".",
+    'debug' : True,
+    'xsrf_cookies' : False,
+    'databases' : {
+        'default': {
+            'host' : '127.0.0.1',
+            'port' : 3306,
+            'user' : 'root',
+            'pwd' : 'xxx',
+            'name' : 'k8s_ops',
+        },
+        'readonly': {
+            'host' : '127.0.0.1',
+            'port' : 3306,
+            'user' : 'root',
+            'pwd' : 'xxx',
+            'name' : 'k8s_ops',
+        }
+    },
+    'redises' : {
+        'default': {
+            'host': '127.0.0.1',
+            'port': '6379',
+            'db': 6,
+            'auth': True,
+            'charset': 'utf-8',
+            'password': ''
+        }
+    },
+    'mqs' : {
+        'default': {
+            'MQ_ADDR': '127.0.0.1',
+            'MQ_PORT': 5672,
+            'MQ_VHOST': '/',
+            'MQ_USER': 'sz',
+            'MQ_PWD': '123456',
+        }
+    },
+}
+
+
+jenkins_conf = {
+    'url' : 'http://xxx.com',
+    'user' : 'admin',
+    'pwd' : 'xxx'
+}
